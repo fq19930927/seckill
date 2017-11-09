f73e082ab18b0c2c63e5f7ee64ef6ea2748760e5
fuqiang <18251359382@163.com>
2017-11-09 22:51:41 +0800

    service层编写

---
+++ src/test/java/org/seckill/service/SeckillServiceTest.java
@@ -0,0 +1,76 @@
+package org.seckill.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.seckill.dto.Exposer;
+import org.seckill.dto.SeckillExecution;
+import org.seckill.entity.Seckill;
+import org.seckill.exception.RepeatKillException;
+import org.seckill.exception.SeckillCloseException;
+import org.seckill.exception.SeckillException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//告诉junit spring配置文件
+@ContextConfiguration({"classpath:spring/spring-dao.xml",
+                            "classpath:spring/spring-service.xml"})
+public class SeckillServiceTest {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private SeckillService seckillService;
+
+    @Test
+    public void getSeckillList() throws Exception {
+        List<Seckill> seckillList = seckillService.getSeckillList();
+        logger.info("list={}",seckillList);
+    }
+
+    @Test
+    public void getById() throws Exception {
+        long id = 1000L;
+        Seckill seckill = seckillService.getById(id);
+        logger.info("seckill={}",seckill);
+    }
+
+    //测试代码完整逻辑,注意可重复执行
+    @Test
+    public void exportSeckillUrl() throws Exception {
+        long id = 1000L;
+        Exposer exposer = seckillService.exportSeckillUrl(id);
+        if (exposer.isExposed()) {
+            logger.info("exposer={}", exposer);
+            long phone = 15327863453L;
+            String md5 = exposer.getMd5();
+            try {
+                SeckillExecution seckillExecution = seckillService.executeSeckill(id, phone, md5);
+                logger.info("result={}", seckillExecution);
+            } catch (RepeatKillException e) {
+                logger.error(e.getMessage());
+            } catch (SeckillCloseException e) {
+                logger.error(e.getMessage());
+            }
+        }else{
+            //秒杀未开启
+            logger.warn("exposer={}",exposer);
+            }
+        }
+
+
+    @Test
+    public void executeSeckill() throws Exception {
+        long id = 1000L;
+        long phone = 15435134151L;
+
+    }
+
+}
