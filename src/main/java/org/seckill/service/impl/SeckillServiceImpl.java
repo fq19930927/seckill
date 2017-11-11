682b77d07fd7fec55cefbb00f1942ad60f864e6a
fuqiang <18251359382@163.com>
2017-11-11 21:33:39 +0800

    添加redis中间键,减少对mysql交互

--- src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
+++ src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
@@ -2,6 +2,7 @@ package org.seckill.service.impl;
 
 import org.seckill.dao.SeckillDao;
 import org.seckill.dao.SuccessKilledDao;
+import org.seckill.dao.cache.RedisDao;
 import org.seckill.dto.Exposer;
 import org.seckill.dto.SeckillExecution;
 import org.seckill.entity.Seckill;
@@ -33,6 +34,9 @@ public class SeckillServiceImpl implements SeckillService {
     @Autowired
     private SuccessKilledDao successKilledDao;
 
+    @Autowired
+    private RedisDao redisDao;
+
     //md5盐值,防止用户猜出md5值
     private final String salt = "asdf^*&%^(*(*134FADF";
 
@@ -45,20 +49,35 @@ public class SeckillServiceImpl implements SeckillService {
     }
 
     public Exposer exportSeckillUrl(long seckillId) {
-        Seckill seckill = seckillDao.queryById(seckillId);
-        if (seckill == null){
-            return new Exposer(false, seckillId);
+        //优化点:缓存优化:超时的基础上维护一致性
+        //1。访问redis
+
+        Seckill seckill = redisDao.getSeckill(seckillId);
+        if (seckill == null) {
+            //2.访问数据库
+            seckill = seckillDao.queryById(seckillId);
+            if (seckill == null) {//说明查不到这个秒杀产品的记录
+                return new Exposer(false, seckillId);
+            }else {
+                //3,放入redis
+                redisDao.putSeckill(seckill);
+            }
+
         }
-        Date startTime = seckill.getStartTime();
-        Date endTime = seckill.getEndTime();
+
+
+        //若是秒杀未开启
+        Date startTime=seckill.getStartTime();
+        Date endTime=seckill.getEndTime();
         //系统当前时间
-        Date nowTime = new Date();
-        if (nowTime.getTime() < startTime.getTime() ||
-                nowTime.getTime() > endTime.getTime()){
+        Date nowTime=new Date();
+        if (startTime.getTime()>nowTime.getTime() || endTime.getTime()<nowTime.getTime())
+        {
             return new Exposer(false,seckillId,nowTime.getTime(),startTime.getTime(),endTime.getTime());
         }
-        //转化特定字符串的过程,不可逆
-        String md5 = getMd5(seckillId);
+
+        //秒杀开启，返回秒杀商品的id、用给接口加密的md5
+        String md5=getMd5(seckillId);
         return new Exposer(true,md5,seckillId);
     }
 
